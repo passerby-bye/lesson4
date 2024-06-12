1c1b7d9b7bda9215b9e5aad9c40aaa7420e1397d
passerby-bye <69146963+passerby-bye@users.noreply.github.com>
2024-06-12 18:56:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ os.system(f'git clone https://code.openxlab.org.cn/H2H/lesson4.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
+model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16)
 
 def chat(message,history):
     for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
